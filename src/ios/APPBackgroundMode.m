170b3b17bccc4b8578e5a534cd0d7574d89b3328
Artur Khidirnabiev <node.arche@gmail.com>
2015-12-10 22:39:30 +0300

    Fix

--- src/ios/APPBackgroundMode.m
+++ src/ios/APPBackgroundMode.m
@@ -1,23 +1,23 @@
 /*
-    Copyright 2013-2014 appPlant UG
-
-    Licensed to the Apache Software Foundation (ASF) under one
-    or more contributor license agreements.  See the NOTICE file
-    distributed with this work for additional information
-    regarding copyright ownership.  The ASF licenses this file
-    to you under the Apache License, Version 2.0 (the
-    "License"); you may not use this file except in compliance
-    with the License.  You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing,
-    software distributed under the License is distributed on an
-    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-    KIND, either express or implied.  See the License for the
-    specific language governing permissions and limitations
-    under the License.
-*/
+ Copyright 2013-2014 appPlant UG
+ 
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
 
 #import "APPBackgroundMode.h"
 
@@ -50,24 +50,24 @@ NSString *const kAPPBackgroundEventFailure = @"failure";
 - (void) observeLifeCycle
 {
     NSNotificationCenter* listener = [NSNotificationCenter defaultCenter];
-
+    
     if (&UIApplicationDidEnterBackgroundNotification && &UIApplicationWillEnterForegroundNotification) {
-
+        
         [listener addObserver:self
                      selector:@selector(keepAwake)
                          name:UIApplicationDidEnterBackgroundNotification
                        object:nil];
-
+        
         [listener addObserver:self
                      selector:@selector(stopKeepingAwake)
                          name:UIApplicationWillEnterForegroundNotification
                        object:nil];
-
+        
         [listener addObserver:self
                      selector:@selector(handleAudioSessionInterruption:)
                          name:AVAudioSessionInterruptionNotification
                        object:nil];
-
+        
     } else {
         [self enable:NULL];
         [self keepAwake];
@@ -93,7 +93,7 @@ NSString *const kAPPBackgroundEventFailure = @"failure";
 - (void) disable:(CDVInvokedUrlCommand *)command
 {
     enabled = NO;
-
+    
     [self stopKeepingAwake];
 }
 
@@ -127,15 +127,15 @@ NSString *const kAPPBackgroundEventFailure = @"failure";
  * Let the app going to sleep.
  */
 - (void) stopKeepingAwake {
-
+    
     if (TARGET_IPHONE_SIMULATOR) {
         NSLog(@"BackgroundMode: On simulator apps never pause in background!");
     }
-
+    
     if (audioPlayer.isPlaying) {
         [self fireEvent:kAPPBackgroundEventDeactivate withParams:NULL];
     }
-
+    
     [audioPlayer pause];
 }
 
@@ -145,17 +145,17 @@ NSString *const kAPPBackgroundEventFailure = @"failure";
 - (void) configureAudioPlayer {
     NSString* path = [[NSBundle mainBundle] pathForResource:@"appbeep"
                                                      ofType:@"wav"];
-
+    
     NSURL* url = [NSURL fileURLWithPath:path];
-
-
+    
+    
     audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url
                                                          error:NULL];
-
+    
     // Silent
-    audioPlayer.volume = 100;
+    audioPlayer.volume = 0;
     // Infinite
-    audioPlayer.numberOfLoops = 1;
+    audioPlayer.numberOfLoops = -1;
 };
 
 /**
@@ -164,13 +164,13 @@ NSString *const kAPPBackgroundEventFailure = @"failure";
 - (void) configureAudioSession {
     AVAudioSession* session = [AVAudioSession
                                sharedInstance];
-
+    
     // Play music even in background and dont stop playing music
     // even another app starts playing sound
     [session setCategory:AVAudioSessionCategoryPlayback
              withOptions:AVAudioSessionCategoryOptionMixWithOthers
                    error:NULL];
-
+    
     [session setActive:YES error:NULL];
 };
 
@@ -191,15 +191,15 @@ NSString *const kAPPBackgroundEventFailure = @"failure";
 - (void) fireEvent:(NSString*)event withParams:(NSString*)params
 {
     NSString* active = [event isEqualToString:kAPPBackgroundEventActivate] ? @"true" : @"false";
-
+    
     NSString* flag = [NSString stringWithFormat:@"%@._isActive=%@;",
-                    kAPPBackgroundJsNamespace, active];
-
+                      kAPPBackgroundJsNamespace, active];
+    
     NSString* fn = [NSString stringWithFormat:@"setTimeout('%@.on%@(%@)',0);",
                     kAPPBackgroundJsNamespace, event, params];
-
+    
     NSString* js = [flag stringByAppendingString:fn];
-
+    
     [self.commandDelegate evalJs:js];
 }
 
